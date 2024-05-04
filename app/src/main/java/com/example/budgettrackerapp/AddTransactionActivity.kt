140741abe6f9c1b2c1082eb30029d339ef8d4a08
ihssanehmichou1 <ihssanesafi123@gmail.com>
2024-05-05 00:51:04 +0100

    commit5

--- app/src/main/java/com/example/budgettrackerapp/AddTransactionActivity.kt
+++ app/src/main/java/com/example/budgettrackerapp/AddTransactionActivity.kt
@@ -1,14 +1,74 @@
 package com.example.budgettrackerapp
 
+import android.annotation.SuppressLint
 import android.os.Bundle
+import android.widget.Button
+import android.widget.ImageButton
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.core.widget.addTextChangedListener
+import androidx.room.Room
+import com.google.android.material.textfield.TextInputEditText
+import com.google.android.material.textfield.TextInputLayout
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.launch
 
 class AddTransactionActivity : AppCompatActivity() {
+    @SuppressLint("MissingInflatedId", "SuspiciousIndentation")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
         setContentView(R.layout.activity_add_transaction)
-    }
-}
+        val labelLayout = findViewById<TextInputLayout>(R.id.labelLayout)
+        val amountLayout = findViewById<TextInputLayout>(R.id.amountLayout)
+        val labelInput = findViewById<TextInputEditText>(R.id.labelInput)
+        val amountInput = findViewById<TextInputEditText>(R.id.amountInput)
+        val descriptionInput = findViewById<TextInputEditText>(R.id.descriptionInput)
+        val addTransactionBtn = findViewById<Button>(R.id.addTransactionBtn)
+        val closeBtn = findViewById<ImageButton>(R.id.closeBtn)
+
+                labelInput.addTextChangedListener {
+                    if(it!!.count() > 0)
+                        labelLayout.error = null
+                }
+
+                amountInput.addTextChangedListener {
+                    if(it!!.count() > 0)
+                        amountLayout.error = null
+                }
+
+                addTransactionBtn.setOnClickListener {
+                    val label = labelInput.text.toString()
+                    val description = descriptionInput.text.toString()
+                    val amount = amountInput.text.toString().toDoubleOrNull()
+
+                    if(label.isEmpty())
+                        labelLayout.error = "Please enter a valid label"
+
+                    else if(amount == null)
+                        amountLayout.error = "Please enter a valid amount"
+                    else {
+//                        val transaction  =Transaction(0, label, amount, description)
+//                        insert(transaction)
+                    }
+                }
+
+                closeBtn.setOnClickListener {
+                    finish()
+                }
+            }
+
+//            private fun insert(transaction: Transaction){
+//                val db = Room.databaseBuilder(this,
+//                    AppDatabase::class.java,
+//                    "transactions").build()
+//
+//                GlobalScope.launch {
+//                    db.transactionDao().insertAll(transaction)
+//                    finish()
+//                }
+//            }
+
+        }
--- app/src/main/java/com/example/budgettrackerapp/MainActivity.kt
+++ app/src/main/java/com/example/budgettrackerapp/MainActivity.kt
@@ -1,45 +1,62 @@
 package com.example.budgettrackerapp
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 
 class MainActivity : AppCompatActivity() {
-    private lateinit var transactions : ArrayList<Transaction>
-    private lateinit var transactionsAdapter : TransactionsAdapter
+    private lateinit var transactions: ArrayList<Transaction>
+    private lateinit var transactionsAdapter: TransactionsAdapter
     private lateinit var linearLayoutManager: LinearLayoutManager
-    private lateinit var recyclerview:RecyclerView
+    private lateinit var recyclerview: RecyclerView
+    private lateinit var balance: TextView
+    private lateinit var budget: TextView
+    private lateinit var expense: TextView
+    private lateinit var addBtn: Button
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        balance = findViewById(R.id.balance)
+        budget = findViewById(R.id.budget)
+        expense = findViewById(R.id.expense)
+        recyclerview = findViewById(R.id.recyclerview)
+        addBtn = findViewById(R.id.btn_add)
+
         transactions = arrayListOf(
-            Transaction("Monthly Budget",4000.00),
-            Transaction("Food Budget",-2000.00),
-            Transaction("Medical Budget",-300.00),
-            Transaction("Subscriptions Budget",-85.00),
-            Transaction("Internet Budget",-200.00),
-            Transaction("Car Budget",0.00),
+            Transaction("Monthly Budget", 4000.00),
+            Transaction("Food Budget", -2000.00),
+            Transaction("Medical Budget", -300.00),
+            Transaction("Subscriptions Budget", -85.00),
+            Transaction("Internet Budget", -200.00),
+            Transaction("Car Budget", 0.00),
         )
         transactionsAdapter = TransactionsAdapter(transactions)
         linearLayoutManager = LinearLayoutManager(this)
-        recyclerview = findViewById(R.id.recyclerview)
         recyclerview.adapter = transactionsAdapter
         recyclerview.layoutManager = linearLayoutManager
 
         updateDashboard()
+
+        addBtn.setOnClickListener {
+            val intent = Intent(this, AddTransactionActivity::class.java)
+            startActivity(intent)
+        }
     }
-    private fun updateDashboard(){
+
+    private fun updateDashboard() {
         val totalAmount = transactions.map { it.amount }.sum()
-        val budgetAmount = transactions.filter { it.amount>0 }.map{it.amount}.sum()
+        val budgetAmount = transactions.filter { it.amount > 0 }.map { it.amount }.sum()
         val expenseAmount = totalAmount - budgetAmount
 
-        balance.text = "$ %.2f".format(totalAmount)
-        budget.text = "$ %.2f".format(budgetAmount)
-        expense.text = "$ %.2f".format(expenseAmount)
+        balance.text = "$%.2f".format(totalAmount)
+        budget.text = "$%.2f".format(budgetAmount)
+        expense.text = "$%.2f".format(expenseAmount)
     }
 }
